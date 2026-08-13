eb603014369dc1679e7bc95ee4f5338eaaa8b4b5
knarendrakumar187 <knarendrakumar187@gmail.com>
2026-08-13 15:07:51 +0530

    Time: 3 ms (84.77%) | Memory: 21.3 MB (18.48%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,7 +2,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count=0
         candiate=0
-
+#Moore Voting Algorithm
         for num in nums:
             if count==0:
                 candiate = num
